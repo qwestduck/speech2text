2527324f3824d5a4c37fb7bb0f709e187b98467d
William Panlener <wpanlener@gmail.com>
2012-05-10 09:36:42 -0500

    Alter the training vocabulary

--- train.m
+++ train.m
@@ -27,8 +27,10 @@ function train()
    configData;
    hmmTraining_fe(config.trainingFolder, config );
    organizeObservationsPerWord( config.trainingFolder, config.vocabulary, config.featureType, config.modelFolder );
-   trainHmm( config.modelFolder, 'one', 2, 2 );
-   trainHmm( config.modelFolder, 'two', 2, 2 );
-   trainHmm( config.modelFolder, 'three', 3, 2 );
-   trainHmm( config.modelFolder, 'four', 2, 2 );
+   % trainHmm( config.modelFolder, 'one', 2, 2 );
+   % trainHmm( config.modelFolder, 'two', 2, 2 );
+   % trainHmm( config.modelFolder, 'three', 3, 2 );
+   % trainHmm( config.modelFolder, 'four', 2, 2 );
+   trainHmm( config.modelFolder, 'm', 1, 2 );
+   trainHmm( config.modelFolder, 'f', 1, 2 );
 end
